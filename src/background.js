d7e514e6cd9eef9fc6e75fd8895b527a00412623
Jasper <jasperparker@outlook.com.au>
2021-07-06 01:14:53 +1000

    More visual tweaks

--- src/background.js
+++ src/background.js
@@ -1,10 +1,31 @@
-function apply_changes() {
-  document.body.style.backgroundColor = 'red';
-}
-
-chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: apply_changes
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.local.set({
+    self_followers: ["bpiv", "dltzk"]
   });
 });
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  console.log(tabId);
+  console.log(changeInfo);
+  console.log(tab);
+
+  if (changeInfo.status === "complete") {
+    chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      files: ["./main.js"]
+    }, () => {
+      console.log("ghhhhhhhh");
+    });
+  }
+});
+
+// function apply_changes() {
+//   document.body.style.backgroundColor = 'red';
+// }
+
+// chrome.action.onClicked.addListener((tab) => {
+//   chrome.scripting.executeScript({
+//     target: { tabId: tab.id },
+//     function: apply_changes
+//   });
+// });
--- src/main.js
+++ src/main.js
@@ -2,7 +2,24 @@ function grab_tag(str) {
   return str.substr(str.lastIndexOf('/') + 1);
 }
 
+function wait_for_page(class_name) {
+  //This is fucked but idk how else to make it wait until the element exists
+  return new Promise(resolve => {
+    var checkExist = setInterval(function () {
+      var elem = document.getElementsByClassName(class_name);
+      if (elem.length > 0) {
+        clearInterval(checkExist);
+        resolve(elem);
+      }
+    }, 100);
+  });
+}
 
+
+
+function inset_after(newNode, referenceNode) {
+  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
 
 
 
@@ -34,83 +51,110 @@ function apply_changes() {
   //on page switch
   switch (url[3]) {
     case "messages":
+      const messages_box = document.querySelector("#content > div > div.l-messages-main > div > div.conversation__messages");
+      console.log(messages_box);
+      wait_for_page("textfield__label g-required-label").then(text => {
+        text[0].remove();
+      });
+      wait_for_page("conversationMessages__item sc-clearfix").then(msgs => {
 
-      var msgs = document.getElementsByClassName("conversationMessages__item sc-clearfix");
-      var pre_self = false;
-      for (var i = 0; i < msgs.length; i++) {
 
-        var self = msgs[i].childNodes[0].childNodes[4].childNodes[1].childNodes.length > 1;
-        msgs[i].childNodes[0].childNodes[4].childNodes[1].style.visibility = "hidden";
-        var avatar = msgs[i].childNodes[0].childNodes[0];
-        if (self == pre_self && i != 0) {
-          msgs[i].style.marginTop = "-40px";
-          avatar.style.visibility = "hidden";
+        var full_wid = document.getElementsByClassName("sc-media-content")[0].offsetWidth;
+        var pre_self = false;
+        for (var i = 0; i < msgs.length; i++) {
+
+          var self = msgs[i].childNodes[0].childNodes[4].childNodes[1].childNodes.length > 1;
+          msgs[i].childNodes[0].childNodes[4].childNodes[1].style.visibility = "hidden";
+          var avatar = msgs[i].childNodes[0].childNodes[0];
+          if (self == pre_self && i != 0) {
+            msgs[i].style.marginTop = "-40px";
+            avatar.style.visibility = "hidden";
+          }
+          var date = msgs[i].childNodes[0].childNodes[2].childNodes[0].getAttribute("datetime");
+          msgs[i].childNodes[0].childNodes[2].style.visibility = "hidden";
+          var para_count = msgs[i].childNodes[0].childNodes[4].childNodes[3].childNodes.length;
+          var para_fix = [];
+          for (var ii = 1; ii < para_count - 1; ii++) {
+            var message;
+            if (self) {
+              message = msgs[i].childNodes[0].childNodes[4].childNodes[3].childNodes[ii];
+              message.style.backgroundColor = "#e5e5e5";
+            } else {
+              message = msgs[i].childNodes[0].childNodes[4].childNodes[3].childNodes[ii];
+              message.style.backgroundColor = "#ff5500";
+              message.style.color = "white";
+              message.style.alignSelf = "right";
+              var wid = message.offsetWidth;
+              var offset = (full_wid - wid) - 12;
+              message.style.transform = "translateX(" + offset.toString() + "px)";
+              avatar.style.float = "right";
+            }
+
+            message.style.position = "relative";
+            message.style.borderRadius = "16px";
+            message.style.padding = "10px";
+            message.style.top = "-24px";
+            //check for embeds
+            if (message.childNodes.length > 0 && !message.childNodes[0].hasChildNodes()) {
+              message.style.maxWidth = "265px";
+              message.style.display = "inline-block";
+            }
+            // if (ii < para_count - 2) {
+            //   para_fix.push(message);
+            // }
+          }
+          para_fix.forEach(para => {
+            para.parentNode.insertBefore(document.createElement('br'), para.nextSibling);
+          })
+          pre_self = self;
         }
-        var date = msgs[i].childNodes[0].childNodes[2].childNodes[0].getAttribute("datetime");
-        msgs[i].childNodes[0].childNodes[2].style.visibility = "hidden";
-        var para_count = msgs[i].childNodes[0].childNodes[4].childNodes[3].childNodes.length;
-        var para_fix = [];
-        for (var ii = 1; ii < para_count - 1; ii++) {
-          var message;
-          if (self) {
-            message = msgs[i].childNodes[0].childNodes[4].childNodes[3].childNodes[ii];
-            message.style.backgroundColor = "#e5e5e5";
-          } else {
-            message = msgs[i].childNodes[0].childNodes[4].childNodes[3].childNodes[ii];
-            message.style.backgroundColor = "#ff5500";
-            message.style.color = "white";
-            message.style.alignSelf = "right";
-            avatar.style.float = "right";
-          }
+      });
 
-          message.style.position = "relative";
-          message.style.borderRadius = "16px";
-          message.style.padding = "10px";
-          message.style.top = "-24px";
-          //check for embeds
-          if (!message.childNodes[0].hasChildNodes()) {
-            message.style.maxWidth = "265px";
-            message.style.display = "inline-block";
-          }
-          if (ii < para_count - 1) {
-            para_fix.push(message);
-          }
-        }
-        para_fix.forEach(para => {
-          para.parentNode.insertBefore(document.createElement('br'), para.nextSibling);
-        })
-        pre_self = self;
-      }
+
       break;
     default:
       //follows you
-      if (self_followers.includes(user_tag))
-        document.getElementsByClassName("profileHeaderInfo__userName g-type-shrinkwrap-block g-type-shrinkwrap-large-primary")[0].innerHTML +=
-          "<div style=\"display: inline; background-color: #666666; font-size: 16px; padding: 3px; border-radius: 4px; color: #a6a6a6\">Follows you</div>";
-
-      //new icons
-      var links = document.getElementsByClassName("web-profiles__item");
-      console.log(links.length);
-      for (var i = 0; i < links.length; i++) {
-        var link_elem = links[i].childNodes[0].childNodes[0];
-        //Add email, traktrain, applemusic, beatport
-        if (link_elem.href.includes("discord")) {
-          console.log(link_elem.childNodes);
-          link_elem.childNodes[1].style.background = "url('https://pbs.twimg.com/media/E5L38mPX0AE0FEk?format=jpg&name=4096x4096') no-repeat 50% 50%";
-        }
+      if (self_followers.includes(user_tag)) {
+        wait_for_page("profileHeaderInfo__userName g-type-shrinkwrap-block g-type-shrinkwrap-large-primary")
+          .then((name) => {
+            name[0].innerHTML +=
+              "<div style=\"display: inline; background-color: #666666; font-size: 16px; padding: 3px; border-radius: 4px; color: #a6a6a6\">Follows you</div>";
+          });
       }
 
-      //extra page settings
-      // var elem = document.getElementsByClassName("profileSettings__form")[0];
-      // var css = document.createElement('textarea');
-      // elem.appendChild(css);
+      //new icons
+      wait_for_page("web-profiles__item").then(links => {
+        for (var i = 0; i < links.length; i++) {
+          var link_elem = links[i].childNodes[0].childNodes[0];
+          //Add email, traktrain, applemusic, beatport, spotify subdomain, roblox
+          if (link_elem.href.includes("discord")) {
+            console.log(link_elem.childNodes);
+            link_elem.childNodes[1].style.background = "url('https://pbs.twimg.com/media/E5L38mPX0AE0FEk?format=jpg&name=4096x4096') no-repeat 50% 50%";
+          }
+        }
+      });
+
+      //check if own page
+      if (self_tag.toLowerCase() === user_tag.toLowerCase()) {
+        var edit_button = document.getElementsByClassName("sc-button-edit editProfileButton sc-button sc-button-medium sc-button-responsive editProfileButton--hiddenLabelOnSmallScreen")[0];
+        edit_button.addEventListener("click", () => {
+          //extra page settings
+          wait_for_page("profileSettings__form").then((elem) => {
+            var _new = document.createElement('div');
+            _new.innerHTML = '<div class="g-form-section-head"><h3 class="g-form-section-title">Custom CSS</h3><button type="button" class="hintButton sc-button sc-button-small" tabindex="0" aria-haspopup="true" role="button" aria-owns="dropdown-button-2577"></button>  </div>';
+            var css = document.createElement('textarea');
+            _new.appendChild(css);
+            //inset_after(_new, elem[0]);
+            elem[0].appendChild(_new);
+            console.log(_new);
+          });
+        });
+      }
       break;
   }
-  //colour change for debug
 }
 
 
-//document.onload = apply_changes();
-window.addEventListener("load", function () {
-  apply_changes();
-});
+apply_changes();
+
+console.log("Injected script started");
